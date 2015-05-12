92930e08fee9fb07d1980c17a9c34c85a9e90c7a
Bernabé Borrero <bernabeborrero@gmail.com>
2015-05-12 15:24:28 +0200

    Activity

--- app/src/main/java/com/deltagames/tictacchec/MainMenuActivity.java
+++ app/src/main/java/com/deltagames/tictacchec/MainMenuActivity.java
@@ -1,12 +1,12 @@
 package com.deltagames.tictacchec;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainMenuActivity extends ActionBarActivity {
+public class MainMenuActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
